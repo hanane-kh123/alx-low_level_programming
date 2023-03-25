aaadde634a6a4272eafe28b6e18456c0b85c14ca
hanane <77104372+hanane-kh123@users.noreply.github.com>
2023-03-25 16:13:54 -0700

    add file 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	}
-	else if (n == 0)
+	else if (n == 0 || n == 980)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
