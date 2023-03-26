48f987c062d65f38291d9c72a08f909def6c0eb9
hanane <77104372+hanane-kh123@users.noreply.github.com>
2023-03-26 15:09:27 -0700

    add file 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Print all possible cominations of single-digit numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+		if (a == '9')
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
