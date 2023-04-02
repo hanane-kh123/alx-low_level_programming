3f3b49ab2d6271289fc99582036f6bf6029d4378
hanane <77104372+hanane-kh123@users.noreply.github.com>
2023-04-02 11:03:29 -0700

    add file 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplie two integer
+ * @a: integer
+ * @b: integer
+ * Return: 1 or 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,4 @@
 
 int _isupper(int);
 int _isdigit(int);
+int mul(int a, int b);
