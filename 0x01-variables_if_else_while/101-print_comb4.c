789fdbab8a22fd9f2479f12728ec27ca9236a0ab
hanane <77104372+hanane-kh123@users.noreply.github.com>
2023-04-02 04:59:03 -0700

    add file 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,15 +13,15 @@ int main(void)
 	{
 		for (j = i + 1; j <= '8'; j++)
 		{
-			for ( k= j+1; k<= '9'; k++)
+			for (k = j + 1; k <= '9'; k++)
 			{
 				putchar(i);
-                        	putchar(j);
+				putchar(j);
 				putchar(k);
-                       		if (i == '7' && j == '8' && k=='9')
-                                	break;
-                       		putchar(',');
-                        	putchar(' ');
+				if (i == '7' && j == '8' && k == '9')
+					break;
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
